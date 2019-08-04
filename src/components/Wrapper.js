53fa3334cfc313b965d3d4ee9eee574feafcf8ce
Santiago Minian <minian.santiago@gmail.com>
2019-08-04 17:29:01 -0300

    Add PropTypes

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
@@ -8,7 +9,13 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default ({ children }) => {
+const Wrapper = ({ children }) => {
   const classes = useStyles();
   return <div className={classes.wrapper}>{children}</div>;
 };
+
+Wrapper.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Wrapper;
